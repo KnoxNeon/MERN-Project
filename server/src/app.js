b2d2a65480d00e790c4b2c3371c046c85de0e5ae
KnoxNeon <iftey10@gmail.com>
2025-05-19 20:07:24 +0600

    error handle using http-errors

---
+++ server/src/app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const morgan = require('morgan')
+const createError = require('http-errors')
+
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/user', (req, res) => {
+    res.status(200).send({
+    message: 'user profile is returned'
+  });
+});
+
+//client error
+app.use((req, res, next) => {
+  createError(404, 'route not found')
+  next();
+})
+
+//server error
+app.use((err, req, res, next) => {
+    return res.status(err.status || 500).json({
+        success: false,
+        message: err.message,
+    });
+});
+
+module.exports = app
+
+
+
