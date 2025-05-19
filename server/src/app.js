2c1528c458922e9bf4673cc2f7af5fe69a9041da
KnoxNeon <iftey10@gmail.com>
2025-05-19 20:32:39 +0600

    added api security

--- server/src/app.js
+++ server/src/app.js
@@ -3,8 +3,18 @@ const app = express()
 const port = 3000
 const morgan = require('morgan')
 const createError = require('http-errors')
+// const xssClean = require('xss-clean')
+const rateLimit = require('express-rate-limit')
 
 
+const rateLimiter  = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 5,
+  message: 'Too many requests, try again later',
+});
+
+
+app.use(rateLimiter);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
