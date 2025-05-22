8f21a0b7be7c9dafa267a826434851487958383a
KnoxNeon <iftey10@gmail.com>
2025-05-23 02:03:44 +0600

    untested searching

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,11 +1,28 @@
 const createError = require('http-errors');
+const User = require('../models/userModel');
 
 
-const getUsers = (req, res, next) => {
+const getUsers = async (req, res, next) => {
     try {
+        const search = req.query.search || "";
+        const page = Number(req.query.page) || 1;
+        const limit= Number(req.query.limit) || 5;
+
+        const searchRegExp = new RegExp('.*' + search + '.*', 'i');
+
+        const filter = {
+            isAdmin: {$ne: true},
+            $or: [
+                {name: {$regex: searchRegExp}},
+                {email: {$regex: searchRegExp}},
+                {phone: {$regex: searchRegExp}},
+            ]
+        };
+
+        const users = await User.find(filter)
         res.status(200).send({
             message: 'users were returned',
-           
+            users,
         });
     } catch (error) {
         next(error)
