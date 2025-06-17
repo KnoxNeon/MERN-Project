2bdccdfc320d14df47a26d274e6e71b01b6fcf9e
KnoxNeon <iftey10@gmail.com>
2025-06-17 22:07:12 +0600

    modular jwt

--- server/src/helper/jsonwebtoken.js
+++ server/src/helper/jsonwebtoken.js
@@ -1,9 +1,23 @@
 const jwt = require('jsonwebtoken');
 const createJSONWebToken = (payload, secretKey, expiresIn) => {
-    const token = jwt.sign(payload, secretKey, {expiresIn});
 
-    return token;
-  
+    if (typeof payload !== 'object' || !payload) {
+        throw new Error('Payload must be a non-empty object')
+    };
+
+    if (typeof secretKey !== 'string' || secretKey === '') {
+        throw new Error('SecretKey must be a non-empty string')
+    };
+
+    try {
+        const token = jwt.sign(payload, secretKey, { expiresIn });
+        return token;
+    } catch (error) {
+        console.error("Failed to sign in to JWT:", error);
+        throw error;
+
+    }
+
 }
 
-module.exports = {createJSONWebToken};
+module.exports = { createJSONWebToken };
